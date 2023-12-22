2ef901e7c6695c50e6efa87e7f17526573c679a5
shakawat20 <shakawat.cse20@gmail.com>
2023-12-22 16:31:54 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# journey-junction-server
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config()
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 9000;
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.obhaluk.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    try {
+        client.connect()
+        const database=client.db("journey-junction")
+        const destinations=database.collection("destination")
+      
+
+        app.get('/destination',async(req,res)=>{
+
+            const x=await destinations.find().toArray()
+            res.send(x)
+
+        })
+
+
+
+        app.get('/', async (req, res) => {
+            console.log("hello world")
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+
+
+
+app.listen(PORT, function (err) {
+
+    console.log(`listening at ${PORT}`);
+});
