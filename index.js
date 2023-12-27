51aac4d4f2b457ba3889e02ef3f93ee5c81f6adb
shakawat20 <shakawat.cse20@gmail.com>
2023-12-28 02:11:31 +0600

    hello world

--- index.js
+++ index.js
@@ -21,78 +21,117 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+
+    return res.status(401).send({ message: 'UnAuthorized access' })
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+
+    }
+    else {
+      req.decoded = decoded
+      next()
+    }
+
+  });
+
+}
+
+
+
+
+
 async function run() {
-    try {
-        client.connect()
-        const database=client.db("journey-junction")
-        const destinations=database.collection("destination")
-        const PaymentInformation=database.collection("PaymentInformation")
-      
-
-        app.get('/destination',async(req,res)=>{
-
-            const destination=await destinations.find().toArray()
-            res.send(destination)
-
-        })
-        app.post("/paymentConfirm",async(req,res)=>{
-            const payment=req?.body
-            
-            const confirmPayment =await PaymentInformation.insertOne(payment)
-            console.log(confirmPayment)
-            res.send(confirmPayment)
-        })
-        app.get("/paymentInfo/:id",async(req,res)=>{
-          const email=req?.params?.id;
-          const findEmail={email:email}
-            const confirmPayment =await PaymentInformation.find(findEmail).toArray()
-            console.log(confirmPayment)
-            res.send(confirmPayment)
-
-        })
+  try {
+    client.connect()
+    const database = client.db("journey-junction")
+    const destinations = database.collection("destination")
+    const PaymentInformation = database.collection("PaymentInformation")
 
 
-        app.post("/create-payment-intent", async (req, res) => {
-            const { price } = req?.body;
-           
-            // Create a PaymentIntent with the order amount and currency
-            const paymentIntent = await stripe.paymentIntents.create({
-              amount:price*100,
-              currency: "usd",
-              // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-              "payment_method_types": [
-                "card"
-            ]
-            });
-          
-            res.send({
-              clientSecret: paymentIntent.client_secret,
-            });
-          });
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = await jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      console.log("khan", token)
+      res.send({ token })
+
+    })
 
 
 
 
+    app.get('/destination', verifyJWT, async (req, res) => {
+
+      const destination = await destinations.find().toArray()
+      res.send(destination)
+
+    })
+
+    app.post("/paymentConfirm", async (req, res) => {
+      const payment = req?.body
+
+      const confirmPayment = await PaymentInformation.insertOne(payment)
+      console.log(confirmPayment)
+      res.send(confirmPayment)
+    })
+
+    app.get("/paymentInfo/:id", async (req, res) => {
+      const email = req?.params?.id;
+      const findEmail = { email: email }
+      const confirmPayment = await PaymentInformation.find(findEmail).toArray()
+      console.log(confirmPayment)
+      res.send(confirmPayment)
+
+    })
+
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req?.body;
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: price * 100,
+        currency: "usd",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        "payment_method_types": [
+          "card"
+        ]
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
 
 
-     
-  
-    }
-    finally {
 
-    }
+
+
+
+
+
+  }
+  finally {
+
+  }
 }
 run().catch(console.dir)
 
 
 app.get('/', async (req, res) => {
-    console.log("hello world")
-    res.send("hello emon")
+
+  res.send("hello world")
 })
 
 
 app.listen(PORT, function (err) {
 
-    console.log(`listening at ${PORT}`);
+  console.log(`listening at ${PORT}`);
 });
