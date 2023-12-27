cffbb24d2b1c7b159f3bf960aacb4180ebb3bac7
shakawat20 <shakawat.cse20@gmail.com>
2023-12-28 05:22:29 +0600

    google

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ async function run() {
 
 
     app.post('/jwt', async (req, res) => {
-      const user = req.body;
+      const user = req?.body;
+      console.log(user)
       const token = await jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
       console.log("khan", token)
       res.send({ token })
